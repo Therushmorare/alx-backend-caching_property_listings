8e8da80d131edc975284a7e3986766ae09f0df1c
Therushmorare <tshego.tt.morare@gmail.com>
2025-08-31 21:31:17 +0000

    task 1

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property  # assuming you have a Property model
 
-# Create your views here.
+@cache_page(60 * 15)  # Cache for 15 minutes
+def property_list(request):
+    properties = list(Property.objects.values())  # get all properties as dicts
+    return JsonResponse(properties, safe=False)
+
